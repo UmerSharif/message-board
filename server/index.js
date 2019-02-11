f238ccea2de93e2b385ce7a572156daeec2a0011
umer khan <umer.kdev@gmail.com>
2019-02-12 01:20:56 +0200

    setting up db

--- server/index.js
+++ server/index.js
@@ -3,14 +3,23 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 
+const messages = require('./db/messages')
+
 const app = express()
 
 app.use(morgan('tiny'))
 app.use(cors())
 app.use(bodyParser.json())
 
+
+app.get('/messages', (req, res) => {
+    messages.getAll().then(messages => {
+        res.json(messages)
+    })
+})
+
 app.get('/', (req, res) => {
-    res.send({
+    res.json({
         message : 'hey dude it works'
     })
 })
